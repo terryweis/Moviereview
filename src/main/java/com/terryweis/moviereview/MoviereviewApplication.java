4a7d917171be40481735a1fa646ffb452186330b
terryweis <weist2088@gmail.com>
2024-06-11 09:38:05 -0500

    added MongoDB

--- src/main/java/com/terryweis/moviereview/MoviereviewApplication.java
+++ src/main/java/com/terryweis/moviereview/MoviereviewApplication.java
@@ -2,12 +2,21 @@ package com.terryweis.moviereview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviereviewApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviereviewApplication.class, args);
+	}
+
+	@GetMapping("/")
+
+	public String apiRoot(){
+		return "Hello World";
 	}
 
 }
