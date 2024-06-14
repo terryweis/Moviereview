3a9884b13c20e0f2a14e94498dcb49b35d4f6e1b
terryweis <weist2088@gmail.com>
2024-06-14 10:45:32 -0500

    Added Movie Controller, Repo, and Service

---
+++ src/main/java/com/terryweis/moviereview/MovieService.java
@@ -0,0 +1,15 @@
+package com.terryweis.moviereview;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+}
