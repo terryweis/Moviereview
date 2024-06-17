37f2930c265229c6645158207e4628febb578fda
terryweis <weist2088@gmail.com>
2024-06-17 12:26:57 -0500

    Added Review Repo, Service, and controller

--- src/main/java/com/terryweis/moviereview/MovieController.java
+++ src/main/java/com/terryweis/moviereview/MovieController.java
@@ -1,15 +1,18 @@
 package com.terryweis.moviereview;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -19,8 +22,12 @@ public class MovieController {
     private MovieService movieService;
 
     @GetMapping
-    public ResponseEntity<List<Movie>> getAllmovies(){
+    public ResponseEntity<List<Movie>> getAllMovies(){
         return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 
+    @GetMapping("/{imdbId}")
+    public ResponseEntity <Optional<Movie>>getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
 }
